b9e187898c1f02cb7b5a4c4a4545eb91b45fd2c3
Xenon Zenon <jayson_abad@outlook.com>
2023-03-20 04:18:39 +0800

    Added CSharp

---
+++ CSharp/test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TestCode
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter username: ");
+      string username = Console.ReadLine();
+      Console.WriteLine("Username is: " + username);
+    }
+  }
+}
